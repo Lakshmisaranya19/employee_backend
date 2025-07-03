f53285450809ec112d813f661f3df89a979646ef
Lakshmi Saranya Dhulipala <dlakshmisarany678@gmail.com>
2025-07-03 13:41:50 +0530

    Created User model with SQLAlchemy

---
+++ app/models/users.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String, Boolean, DateTime, func
+from app.db import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    is_active = Column(Boolean, default=True)
+    created_at = Column(DateTime, default=func.now())
+    updated_at = Column(DateTime, default=func.now(), onupdate=func.now())
